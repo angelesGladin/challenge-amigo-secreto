cd7d0e0ce280f9a6129ef13c034f8842dee7a14e
Angeles Morales <marangladin@gmail.com>
2025-02-07 12:33:08 -0600

    implementación de función para actualizar lista y sortear los amigos

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let amigos = [];
+let indiceAleatorio = 0;
+let amigoSorteado = 0;
+
 
 function agregarAmigo() {
     //capturo nombre y limpio texto inicil (trimp)
-    let capturaNombreAmigo = document.getElementById('amigo', String).value.trim();
+    let capturaNombreAmigo = document.getElementById('amigo').value.trim();
 
     //validar la entrada
     if (capturaNombreAmigo === "") {
@@ -12,16 +15,19 @@ function agregarAmigo() {
     //agregar nombre al array 
     amigos.push(capturaNombreAmigo);
     //limpia el campo
-    document.getElementById('amigo', String).value = "";
+    document.getElementById('amigo').value = "";
 
-
+    mostrarListaAmigos()
     console.log(amigos);
 }
 
 function mostrarListaAmigos(){
+    //llamar la lista con su id
     let lista = document.getElementById("listaAmigos");
+    //limpia la lista existente par que no se duplique 
     lista.innerHTML = ""
 
+    //iteración con el arreglo y creación de li
     for (let amigo of amigos) {
         let li = document.createElement('li');
         li.textContent = amigo;
@@ -29,3 +35,17 @@ function mostrarListaAmigos(){
     }
 
 }
+    function sortearAmigo() {
+        //validar que la lista no esste vacia 
+        if(amigos.length === 0) {
+            alert('No hay amigos en la lista para sortear.');
+            return;
+        }
+        //genera nombre aleatorio
+        indiceAleatorio = Math.floor(Math.random()*amigos.length++);
+        //obtener nombre
+        amigoSorteado = amigos[indiceAleatorio]; 
+        //mostrar resultado 
+        document.getElementById("sortear").innerHTML = `El amigo seleccionado: <strong>${amigoSorteado}</strong>`;
+    }
+    
